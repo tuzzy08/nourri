f2ced45bd54d3081485cd9782b49e9094400692d
xavier <tuzzy08@gmail.com>
2024-03-20 20:39:40 +0100

    Added additional module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -17,6 +20,9 @@ import { ConfigModule } from '@nestjs/config';
       uri: process.env.AMQP_URL,
       connectionInitOptions: { wait: false },
     }),
+    UsersModule,
+    AuthModule,
+    OrdersModule,
   ],
 })
 export class AppModule {}
